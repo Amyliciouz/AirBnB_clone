602de8bb9816cb806d5ac9f1c41005a938c4f6bb
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-29 03:03:26 -0700

    task 4  complete

--- models/base_model.py
+++ models/base_model.py
@@ -19,3 +19,16 @@ class BaseModel:
         """prints [<class name>] (<self.id>) <self.__dict__>"""
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                 self.id, self.__dict__))
+
+    def save(self):
+        """ updates updated_at attribute"""
+        self.updated_at = datetime.now()
+        return self.updated_at
+
+    def to_dict(self):
+        """returns a dict containing all key,value of dict"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
