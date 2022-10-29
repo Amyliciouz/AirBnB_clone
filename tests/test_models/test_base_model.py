1ed4391a359a2ee0210baaed519a61811c5ab9c8
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-29 17:44:57 +0000

    test model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+""" Unittest Module for base_model class"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))i
+
+class Test_BaseModel(unittest.TestCase):
+	"""unittest class of BaseModel, inheriting from unittest module"""
+
