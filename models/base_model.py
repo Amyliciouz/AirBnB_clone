4ffbb83844584c6daec378276adb45776f79e184
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-29 01:00:39 -0700

    correct pycodestyle errors

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,15 @@
 import uuid
 import datetime
 
-""" The Base Model : contains instances and methods 
-for other classes """
+"""The Base Model:contains instances and methods
+for other classes"""
 
 
 class BaseModel:
-	""" class definition """
-	
-	def __init__(self):
-	""" class instantiation"""
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+    """ class definition """
+
+    def __init__(self):
+        """ class instantiation"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
