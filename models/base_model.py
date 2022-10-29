8eb84b276701ade9298961e8f29b3ca06ddcb7aa
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-29 07:35:15 +0000

    define and instantiate Base Model

---
+++ models/base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+""" The Base Model : contains instances and methods 
+for other classes """
+
+
+class BaseModel:
+	""" class definition """
+	
+	def __init__(self):
+	""" class instantiation"""
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
