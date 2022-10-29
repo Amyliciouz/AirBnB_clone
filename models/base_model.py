36b53670e0d32f5ae296f6157db22a4b9762a867
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-29 02:04:10 -0700

    str method task

--- models/base_model.py
+++ models/base_model.py
@@ -14,3 +14,8 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def __str__(self):
+        """prints [<class name>] (<self.id>) <self.__dict__>"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
