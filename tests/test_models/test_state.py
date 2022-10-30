5323c77390a95befe0686751ae6c849995a06eb6
nimikadejo <kadejotoluwanimi@gmail.com>
2022-10-30 14:57:42 -0700

    final correction for pycodestyle errs

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,7 +6,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-import unittest 
+import unittest
 
 
 class test_state(unittest.TestCase):
